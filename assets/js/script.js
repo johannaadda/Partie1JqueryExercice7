57c142df8f01c424290a997297a69e7b89402a5b
johannaadda <johannaadda@icloud.com>
2020-05-10 12:22:26 -0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+$(function (){
+  // On ajoute l'evenement onclick  à notre element ID "button pour qu au clik une boite de dialogue s'ouvre
+  $('#button').click(function(){
+    // / Afin de creer une boite de dialogue, utilisation d'une variable  en attribuant une valeur 'Doe' (message sera affiché dans )
+  alert('Bonjour');
+});
+});
